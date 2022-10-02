679437bad9c455bb9636ded54378e84083c17e79
Vladimir <vladimirg@moonsite.co.il>
2022-10-02 23:19:09 +0300

    added adblocker extension

---
+++ adblock/src/contentScript/contentScript.ts
@@ -0,0 +1,22 @@
+const rules: { [url: string]: () => void } = {
+  'https://www.nytimes.com/section/technology': filterByClassname,
+};
+
+function filterNYT() {
+  const app = document.getElementById('site-content');
+  const wrapper = document.getElementById('top-wrapper');
+  app.removeChild(wrapper);
+}
+
+function filterByClassname() {
+  const divs = document.getElementsByTagName('div');
+  for (const div of divs) {
+    if (div.className.indexOf('ad') !== -1) {
+      div.style.display = 'none';
+    }
+  }
+}
+
+if (document.URL in rules) {
+  rules[document.URL]();
+}
